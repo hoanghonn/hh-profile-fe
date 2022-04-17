c33d43eb89afc5525d7b40799410343350f747a1
Hoang <hoangnguyenthai97@gmail.com>
2022-04-17 15:45:20 +0700

    Add tailwind; Update layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,26 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div>
+      <img src="" alt="Background" />
+      <h1>HELLO WOLRD</h1>
+      <h2>I’M HOANG NGUYEN. SOFTWARE ENGINEER, TECH ENTHUSIAST AND CREATOR</h2>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
   </Layout>
 </template>
 
 <script>
+import Layout from "../layouts/Layout.vue";
 export default {
+  components: { Layout },
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Hello, world!",
+  },
+};
 </script>
 
 <style>
